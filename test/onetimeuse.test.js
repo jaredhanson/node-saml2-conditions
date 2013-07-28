321738bd41dc6b2ec24d1f78a117a538993c10d2
Jared Hanson <jaredhanson@gmail.com>
2013-07-28 14:18:07 -0700

    Add support for OneTimeUse condition.

---
+++ test/onetimeuse.test.js
@@ -0,0 +1,13 @@
+var conditions = require('index')
+  , fs = require('fs');
+
+describe('SAML assertion with a one time use condition', function() {
+    
+  var xml = fs.readFileSync(__dirname + '/data/one-time-use.xml', 'utf8');
+  
+  it('should be valid', function() {
+    var ok = conditions.evaluate(xml, { audience: 'http://lucy.localtunnel.me/' });
+    expect(ok).to.be.true;
+  });
+  
+});
