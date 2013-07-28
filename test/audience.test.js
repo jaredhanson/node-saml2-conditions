5e40a60c04fcbf350a37b38c5f91eb5189c28c70
Jared Hanson <jaredhanson@gmail.com>
2013-07-28 14:03:19 -0700

    Fail with descriptive error message.

--- test/audience.test.js
+++ test/audience.test.js
@@ -25,6 +25,12 @@ describe('SAML assertion with an audience restriction', function() {
     expect(ok).to.be.true;
   });
   
+  it('should return an error when failWithError is set', function() {
+    var ok = conditions.evaluate(xml, { audience: 'http://foo.example.com/', failWithError: true });
+    expect(ok).to.be.an.instanceof(Error);
+    expect(ok.message).to.be.equal("SAML assertion not intended for audience: http://lucy.localtunnel.me/");
+  });
+  
 });
 
 describe('SAML assertion with an audience restriction', function() {
@@ -44,6 +50,12 @@ describe('SAML assertion with an audience restriction', function() {
   it('should be invalid if not intended for the audience', function() {
     var ok = conditions.evaluate(xml, { audience: 'http://foo.example.com/' });
     expect(ok).to.be.false;
+  });
+  
+  it('should return an error when failWithError is set', function() {
+    var ok = conditions.evaluate(xml, { audience: 'http://foo.example.com/', failWithError: true });
+    expect(ok).to.be.an.instanceof(Error);
+    expect(ok.message).to.be.equal("SAML assertion not intended for audience: http://lucy.localtunnel.me/ http://samy.localtunnel.me/");
   });
   
 });
