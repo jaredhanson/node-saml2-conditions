8f1169c09c81ab9e51f751e84e418d1d50c15e8c
Jared Hanson <jaredhanson@gmail.com>
2013-07-28 17:15:41 -0700

    Expose Evaluator and checks.

--- test/module.test.js
+++ test/module.test.js
@@ -6,4 +6,15 @@ describe('saml2-conditions', function() {
     expect(conditions.evaluate).to.be.a('function');
   });
   
+  it('should export Evaluator constructor', function() {
+    expect(conditions.Evaluator).to.be.a('function');
+  });
+  
+  it('should export checks', function() {
+    expect(Object.keys(conditions.checks)).to.have.length(3);
+    expect(conditions.checks.audienceRestriction).to.be.a('function');
+    expect(conditions.checks.oneTimeUse).to.be.a('function');
+    expect(conditions.checks.proxyRestriction).to.be.a('function');
+  });
+  
 });
